2976ba2fa61418245df067f4c45df92c2a8ce2e0
charlesguo <charles.j.guo@gmail.com>
2016-04-08 15:29:58 +0800

    Skeletal - index working

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var router = require('./config/routes');
+var morgan = require('morgan');
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use('/api',router);
+
+app.listen(port);
+console.log(`server started on port ${port}`);
---
+++ config/routes.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const data = require('../data.json');
+// var lease = require('../leases/lease');
+
+// INDEX
+router.get('/transactions',(req,res) =>
+{
+	res.json(data);
+})
+
+// app.get('/api/highscores', (req,res) => {
+//   res.json(data);
+// })
+//
+// router.post('/',(req,res) =>
+// {
+// 	var result = candy.create(req.body.name,req.body.color);
+// 	res.status(result.status).json(result.value);
+// })
+//
+// router.get('/:id',(req,res) =>
+// {
+// 	var result = candy.show(req.params.id);
+// 	res.status(result.status).json(result.value);
+// })
+//
+// router.put('/:id',(req,res) =>
+// {
+// 	var result = candy.update(req.params.id,req.body.name,req.body.color);
+// 	res.status(result.status).json(result.value);
+// })
+//
+// router.delete('/:id',(req,res) =>
+// {
+// 	var result = candy.destroy(req.params.id);
+// 	res.status(result.status).json(result.value);
+// })
+
+module.exports = router;
