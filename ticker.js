2eb358ab5a52574d5a553047b74e3a5096ed538f
charlesguo <charles.j.guo@gmail.com>
2016-04-11 17:43:33 +0800

    added seed file and seeded

---
+++ ticker.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var tickerSchema = mongoose.Schema({
+  name: String,
+  date: Date,
+  price: Number,
+  level: Number,
+  open: Number,
+  high: Number,
+  low: Number,
+  close: Number,
+  volume: Number
+});
+
+module.exports = mongoose.model('Ticker', tickerSchema);
