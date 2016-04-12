7416707737fa591f5a81974825ffd509ba998cd9
user <user@users-MacBook-Pro.local>
2016-04-12 17:06:15 +0800

    Added the Heatmap Views

--- app.js
+++ app.js
@@ -1,56 +1,49 @@
-var express = require("express");
-var app = express();
-var mongoose = require('mongoose');
+var express       = require("express");
+var app           = express();
+var port          = process.env.PORT || 3000;
+var ejs           = require("ejs");
+var path          = require("path");
+var router        = require('./config/routes');
+var morgan        = require('morgan');
+var bodyParser    = require("body-parser");
+
+var mongoose      = require('mongoose');
 mongoose.connect('mongodb://localhost/tradist');
-var port = process.env.PORT || 3000;
-// var router = require('./config/routes');
-var morgan = require('morgan');
-var bodyParser = require("body-parser");
-var Ticker = require('./models/ticker');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+//app.use('/api',router);
+
+
+var Ticker = require('./models/Ticker');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 // app.use('/api',router);
 
-app.get('/ticker/:name/:date', function(req, res) {
-  Ticker.find({name: req.params.name,}, function (err, data) {
-    // if (err)
+// Set up our app to accept to use EJS
 
+
+
+app.get('/api/ticker/:name', function(req, res) {
+  Ticker.find({name: req.params.name}, function (err, data) {
+    if(err) res.json({message: 'Could not find stocks b/c:' + err});
+
+    // if this is just the API, we will respond this msg
     res.json(data);
   });
-    // var data = {
-    //     "ticker": {
-    //         "name": req.params.name,
-    //         "date": req.params.date
-    //     }
-    // };
 });
 
-// app.get('/ticker', function(req, res) {
-//     var tick = Ticker.find();
-//
-//     var data = {
-//         "ticker": {
-//             "name": req.params.name,
-//             "date": req.params.date
-//         }
-//     };
-//     console.log(tick);
-//     res.json(tick);
-//
-// });
-
-app.get('/ticker/:name', function(req, res) {
-    var data = {
-        "ticker": {
-            "name": req.params.name,
-            "date": req.params.date
-        }
-    };
-    console.log(data);
-    res.json(data);
-
+app.get('/heatmap', function(req, res) {
+    res.render('heatmap');
 });
 
 app.listen(port);
