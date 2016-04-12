3b6add8e1cc3d8ac0fd5f2daf98d1e2973c65b1b
Hakim Ariffin <hakimishere@gmail.com>
2016-04-12 16:56:35 +0800

    Pre-coordinated Pushing To Git

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// var lease = require('../leases/lease');
 
 var Bubble = require("../models/bubbles");
 
@@ -9,42 +8,12 @@ var Bubble = require("../models/bubbles");
 router.get('/bubbles', function(req,res) {
 		Bubble.find({}, function(err, stocks) {
 		if (err) res.json({Message: "Stocks cannot be found."});
-
 		res.json(stocks);
-		// res.render('index');
 		});
 });
 
 router.get('/bubblechart', function(req, res){
 		res.render('index.html');
 });
-
-// app.get('/api/highscores', (req,res) => {
-//   res.json(data);
-// })
-//
-// router.post('/',(req,res) =>
-// {
-// 	var result = candy.create(req.body.name,req.body.color);
-// 	res.status(result.status).json(result.value);
-// })
-//
-// router.get('/:id',(req,res) =>
-// {
-// 	var result = candy.show(req.params.id);
-// 	res.status(result.status).json(result.value);
-// })
-//
-// router.put('/:id',(req,res) =>
-// {
-// 	var result = candy.update(req.params.id,req.body.name,req.body.color);
-// 	res.status(result.status).json(result.value);
-// })
-//
-// router.delete('/:id',(req,res) =>
-// {
-// 	var result = candy.destroy(req.params.id);
-// 	res.status(result.status).json(result.value);
-// })
 
 module.exports = router;
