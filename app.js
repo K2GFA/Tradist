5e43fb25ce1b46e1605239b881fff9ab7b74430e
user <user@users-MacBook-Pro.local>
2016-04-12 11:20:04 +0800

    Added Data in Tradist Database, ticker collection

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 var express = require("express");
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tradist');
 var port = process.env.PORT || 3000;
-var router = require('./config/routes');
+// var router = require('./config/routes');
 var morgan = require('morgan');
 var bodyParser = require("body-parser");
+var Ticker = require('./models/ticker');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
-app.use('/api',router);
+// app.use('/api',router);
+
+app.get('/ticker/:name/:date', function(req, res) {
+  Ticker.find({name: req.params.name,}, function (err, data) {
+    // if (err)
+
+    res.json(data);
+  });
+    // var data = {
+    //     "ticker": {
+    //         "name": req.params.name,
+    //         "date": req.params.date
+    //     }
+    // };
+});
+
+// app.get('/ticker', function(req, res) {
+//     var tick = Ticker.find();
+//
+//     var data = {
+//         "ticker": {
+//             "name": req.params.name,
+//             "date": req.params.date
+//         }
+//     };
+//     console.log(tick);
+//     res.json(tick);
+//
+// });
+
+app.get('/ticker/:name', function(req, res) {
+    var data = {
+        "ticker": {
+            "name": req.params.name,
+            "date": req.params.date
+        }
+    };
+    console.log(data);
+    res.json(data);
+
+});
 
 app.listen(port);
 console.log(`server started on port ${port}`);
